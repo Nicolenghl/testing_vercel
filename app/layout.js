61b5a93c2de4032720ae3b1d40a6448e5b865f7e
Nicole <ngnicole.hl@gmail.com>
2025-05-04 21:39:30 +0800

    fix: updated the whole project

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,15 @@
+import './global.css';
+import { Web3Provider } from './context/Web3Context';
+import Navbar from './components/Navbar';
+
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body>
-                {children}
+                <Web3Provider>
+                    <Navbar />
+                    {children}
+                </Web3Provider>
             </body>
         </html>
     );
